a11f3adaec9dd12572f21b0c6021f45c5b6e5d0a
trevans24 <trevans24@gmail.com>
2017-05-06 12:57:12 -0600

    adding files, firebase added, keys for calendar...working on how auth works

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,9 @@
-angular.module('calendarApp', [ui.router]);
+angular.module('calendarApp', ["ui.router", "firebase", "ui.calendar"])
+	.controller('fireController', fireController);
+
+fireController.$inject = ['$scope', '$firebaseObject'];
+
+function fireController($scope, $firebaseObject){
+	let ref = firebase.database().ref();
+	$scope.data = $firebaseObject(ref);
+}
